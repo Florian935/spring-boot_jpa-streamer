c84470c5bc383af843ff19bb821b8fea9ae4da18
florian935 <florian.martin63000@gmail.com>
2021-08-08 19:40:33 +0200

    Feat: initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Spring Boot app with JPA Streamer
+
+## Goal
+
+Goal of this project is to demonstrate how to use JPA Streamer with a Spring Boot application
---
+++ src/test/java/com/florian935/jpastreamer/JpaStreamerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.florian935.jpastreamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaStreamerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
