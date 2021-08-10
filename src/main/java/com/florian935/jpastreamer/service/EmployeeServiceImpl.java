8b279eff9ff37b811b1d5c2a4d730f4918c114b1
florian935 <florian.martin63000@gmail.com>
2021-08-10 23:55:24 +0200

    Feat: remove unused imports

--- src/main/java/com/florian935/jpastreamer/service/EmployeeServiceImpl.java
+++ src/main/java/com/florian935/jpastreamer/service/EmployeeServiceImpl.java
@@ -13,7 +13,6 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.groupingBy;
 import static lombok.AccessLevel.PRIVATE;
